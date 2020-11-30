46c8136a95683fc1819c22e0f4b087fbf0d78394
luzhongkuan <luzhongk@126.com>
2020-11-30 15:40:01 +0800

    fix: dom type.dts

--- packages/dom/src/index.d.ts
+++ packages/dom/src/index.d.ts
@@ -1,32 +1,32 @@
 declare module '@halobear/dom' {
-  interface Fn {
-    append: () => any,
-    remove: () => any,
-    find: () => any,
-    addClass: () => any,
-    removeClass: () => any,
-    eq: () => any,
-    show: () => any,
-    hide: () => any,
-    css: () => any,
-    on: () => any,
-    off: () => any,
-    index: () => any,
-    offAll: () => any,
-    val: () => any,
-    attr: () => any,
-    [key: string]: (...args: any[]) => any 
+  type H = HTMLElement[]
+  interface Ret extends H {
+    append: (el: HTMLElement[] | HTMLElement) => Ret,
+    remove: () => Ret,
+    addClass: (className: string) => Ret,
+    removeClass: (className: string) => Ret,
+    eq: (index: number) => Ret,
+    show: () => Ret,
+    hide: () => Ret,
+    html: (html: string) => Ret,
+    css: (obj: Record<string, string>) => Ret,
+    on: (eventType: string, selector: string|Handler, handler:Handler|undefined) => Ret,
+    off: (eventType: string, selector: string|Handler, handler:Handler|undefined) => Ret,
+    index: () => Ret,
+    offAll: () => Ret,
+    val: () => Ret,
+    attr: () => Ret,
   }
-
+  type Handler = ((...args:any[]) => Ret)
   interface Bala {
     (
       element?: string | HTMLElement | HTMLElement[] | Node | Node[] | null, context?: string | HTMLElement
-    ): HTMLElement[];
+    ): Ret;
     one: (
       element?: string | HTMLElement | HTMLElement[] | Node | Node[] | null, context?: string | HTMLElement
     ) => HTMLElement | undefined;
     // TODO how to define methods?
-    fn: Fn,
+    fn: { [key: string]: (...args: any[]) => any }
     noop: () => void,
     getStyle: (el:HTMLElement, styleProp:string) => string
   }
