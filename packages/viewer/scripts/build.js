5c922351cdff4107589aa0fdfec026d01137f39c
luzhongkuan <luzhongk@126.com>
2021-03-04 16:46:51 +0800

    chore: build

--- packages/viewer/scripts/build.js
+++ packages/viewer/scripts/build.js
@@ -1,94 +1,15 @@
-const path = require('path')
-const fs = require('fs')
-const rollup = require('rollup')
-const { default: resolve } = require('@rollup/plugin-node-resolve')
-const typescript = require('rollup-plugin-typescript2')
-const Terser = require('terser')
-const commonjs = require('@rollup/plugin-commonjs')
-const postcss = require('rollup-plugin-postcss')
+/*
+NODE_ENV=production node scripts/build --formats esm -t // build
+*/
 
-const pkg = require('../package.json')
+const path = require("path")
+const fs = require("fs-extra")
+const chalk = require("chalk")
+const execa = require('execa')
 
-const isDev = process.env.NODE_ENV === 'production'
-const outDir = isDev ? 'lib' : 'public'
+const args = require("minimist")(process.argv.slice(2))
+const formats = args.formats || args.f
+const sourceMap = args.sourcemap || args.s
+const buildTypes = args.types || args.t
 
-const name = (pkg.name.includes('/') ? pkg.name.split('/')[1] : pkg.name).replace(
-  /-(\w)/,
-  (_, $1) => $1.toUpperCase()
-)
-
-const date = {
-  day: new Date().getDate(),
-  month: 'January February March April May June July August September October November December'.split(
-    ' '
-  )[new Date().getMonth()],
-  year: new Date().getFullYear(),
-}
-
-const version = process.env.VERSION || pkg.version
-
-const banner = `
-/**
- * ${pkg.name} ${version}
- * Released on: ${date.month} ${date.day}, ${date.year}
- */
-`.trim()
-
-function buildUMD() {
-  rollup
-    .rollup({
-      input: path.resolve(__dirname, '../src/index.ts'),
-      plugins: [resolve(), commonjs(), postcss({ plugins: [require('autoprefixer')] }), typescript()],
-      external: ['axios'],
-    })
-    .then((bundle) => {
-      return bundle.write({
-        strict: true,
-        name,
-        format: 'umd',
-        file: path.resolve(__dirname, `../${outDir}/index.umd.js`),
-        banner,
-        globals: { axios: 'axios' },
-        exports: 'auto',
-      })
-    })
-    .then((bundle) => {
-      const result = bundle.output[0]
-      const minified = Terser.minify(result.code, {
-        sourceMap: {
-          filename: `index.umd.min.js`,
-        },
-        output: {
-          preamble: banner,
-        },
-      })
-      fs.writeFileSync(path.resolve(__dirname, `../${outDir}/index.umd.min.js`), minified.code)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-
-function buildESM() {
-  rollup
-    .rollup({
-      input: path.resolve(__dirname, '../src/index.ts'),
-      plugins: [resolve(), commonjs(), postcss({ plugins: [require('autoprefixer')] }), typescript()],
-      external: ['axios'],
-    })
-    .then((bundle) => {
-      return bundle.write({
-        strict: true,
-        format: 'esm',
-        file: path.resolve(__dirname, `../${outDir}/index.esm.js`),
-        sourcemap: false,
-        banner,
-      })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-
-buildUMD()
-buildESM()
+console.log(args)
--- packages/viewer/src/core/util.ts
+++ packages/viewer/src/core/util.ts
@@ -74,56 +74,6 @@ export function hideViewer(container: HTMLDivElement) {
   })
 }
 
-// 获取translate
-// export function getTranslate(el: HTMLElement, axis = 'x') {
-//   let matrix
-//   let curTransform
-//   let transformMatrix
-
-//   const curStyle = window.getComputedStyle(el, null)
-
-//   if ('WebKitCSSMatrix' in window) {
-//     curTransform = curStyle.transform || curStyle.webkitTransform
-//     if (curTransform.split(',').length > 6) {
-//       curTransform = curTransform
-//         .split(', ')
-//         .map((a) => a.replace(',', '.'))
-//         .join(', ')
-//     }
-//     // Some old versions of Webkit choke when 'none' is passed; pass
-//     // empty string instead in this case
-//     transformMatrix = new window.WebKitCSSMatrix(curTransform === 'none' ? '' : curTransform)
-//   } else {
-//     const cs = curStyle as any
-//     transformMatrix =
-//       cs.MozTransform ||
-//       cs.OTransform ||
-//       cs.MsTransform ||
-//       cs.msTransform ||
-//       cs.transform ||
-//       cs.getPropertyValue('transform').replace('translate(', 'matrix(1, 0, 0, 1,')
-//     matrix = transformMatrix.toString().split(',')
-//   }
-
-//   if (axis === 'x') {
-//     // Latest Chrome and webkits Fix
-//     if (window.WebKitCSSMatrix) curTransform = transformMatrix.m41
-//     // Crazy IE10 Matrix
-//     else if (matrix.length === 16) curTransform = parseFloat(matrix[12])
-//     // Normal Browsers
-//     else curTransform = parseFloat(matrix[4])
-//   }
-//   if (axis === 'y') {
-//     // Latest Chrome and webkits Fix
-//     if (window.WebKitCSSMatrix) curTransform = transformMatrix.m42
-//     // Crazy IE10 Matrix
-//     else if (matrix.length === 16) curTransform = parseFloat(matrix[13])
-//     // Normal Browsers
-//     else curTransform = parseFloat(matrix[5])
-//   }
-//   return curTransform || 0
-// }
-
 export function listenOption(capture = false, passive = false) {
   return support.passive ? { passive, capture } : capture
 }
